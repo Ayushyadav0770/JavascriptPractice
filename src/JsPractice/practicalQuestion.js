1b2c7fc6cc7347e0d744d44284fbff152f7d9d41
VERAMASA <veramasa@VERAMASAs-MacBook-Air-3.local>
2026-03-06 23:38:01 +0530

    conditional operators

---
+++ src/JsPractice/practicalQuestion.js
@@ -0,0 +1,20 @@
+// Write a program that determines if a person is eligible to drive or not based on their age and having valid driving license?
+
+// let age = 18;
+// let hasDrivingLicense = true;
+
+// if(age >= 18 && hasDrivingLicense){
+//     document.write("Congratulations! You are eligible to drive")
+// }else{
+//     document.write("You are below 18, Can't drive.")
+// }
+
+// ==========================
+// Let say you have a variable score representing a students score. If the score is greateer than or equal to 60
+// the student passes if it is less than it student fails, use the ternary operator to determine the result.
+
+let score = 65;
+
+let result = score > 60 ? "Pass" : "Fail";
+
+document.write(result);
